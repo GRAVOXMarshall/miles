4eda7244b86afe42518cf0e5928cb000a6216768
GRAVOXCHILE <43478537+GRAVOXCHILE@users.noreply.github.com>
2019-06-04 14:53:15 -0400

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,7 +1,26 @@
 <?php 
+	header('Content-type: application/json');
+	/* comprobamos que el usuario nos viene como un parametro */
+	if(isset($_GET['user']) && intval($_GET['user'])) {
 
-	echo json_encode([
-		'user' : 'Nicolas',
-		'pass' : 123456
-	]);
+		$user_id = intval($_GET['user']); 
+
+		/* conectamos a la bd */
+		//$link = mysql_connect('localhost','usuario','password') or die('No se puede conectar a la BD');
+		//mysql_select_db('nombrebd',$link) or die('No se puede seleccionar la BD');
+
+		/* sacamos los posts de bd */
+		//$query = "SELECT post_title, guid FROM wp_posts WHERE post_author = $user_id AND post_status = 'publish' ORDER BY ID DESC LIMIT $number_of_posts";
+		//$result = mysql_query($query,$link) or die('Query no funcional:  '.$query);
+
+		/* creamos el array con los datos */
+		
+		echo json_encode(array(
+			'result' => 'wena',
+			'mundo' => 'mundo'
+		));
+
+		/* nos desconectamos de la bd */
+		@mysql_close($link);
+	}
  ?>
